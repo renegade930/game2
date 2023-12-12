c403b4bc47375f6e8f7e7933d9e14d7ca0fd7401
renegade930 <cornelbejan7@gmail.com>
2023-12-12 14:56:39 +0200

    added key inputs

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
+var test_array: Array[String] = ['Test', 'Hello', 'Stuff']
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Player.rotation_degrees = 45
+	print($Player.rotation_degrees)
+	
+	print(test_array[1])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print($Player.pos)
+	$Player.rotation_degrees += 10 * delta
 	if $Player.position.x > 1000:
 		$Player.pos.x = 1
+
 
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var pos: Vector2 = Vector2.ZERO
+var speed = 200
 
 func _ready():
 	pass # Replace with function body.
@@ -9,4 +10,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position = pos
-	pos.x += 10
+	pos.x += speed * delta
