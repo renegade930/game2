9195bc3d22db8aa04f616fb9d99e9031d19fd945
renegade930 <cornelbejan7@gmail.com>
2024-01-17 15:43:02 +0200

    12

--- scenes/level/inside.gd
+++ scenes/level/inside.gd
@@ -6,6 +6,6 @@ extends base_level
 func _on_area_2d_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player,"speed",0,0.5)
-	TransitionLayer.change_scene("res://scenes/level/inside.tscn")
+	TransitionLayer.change_scene("res://scenes/level/outside.tscn")
 	#get_tree().change_scene_to_file("res://scenes/level/outside.tscn")
 	#get_tree().change_scene_to_packed(outside_scene)
